c71365aab947a0241e0414b1766b3ebe1a78571f
Amani <A_sedrat@estin.dz>
2023-05-01 03:05:16 +0100

    fixing some issues and adding more rules

--- app.py
+++ app.py
@@ -1,73 +1,65 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# define knowledge base
-knowledge_base = {
-    'rules': [
-        {'condition': ['engine starts', 'fuel tank not empty'], 'action': 'check fuel pump'},
-        {'condition': ['engine starts', 'fuel tank empty'], 'action': 'check fuel level'},
-        {'condition': ['engine starts', 'fuel pump working', 'air filter clean'], 'action': 'no problem found'},
-        {'condition': ['engine starts', 'fuel pump working', 'air filter dirty'], 'action': 'replace air filter'},
-        {'condition': ['engine starts', 'fuel pump not working'], 'action': 'replace fuel pump'},
-        {'condition': ['engine does not start'], 'action': 'check battery'},
-        {'condition': ['battery faulty'], 'action': 'replace battery'},
-        {'condition': ['oil light on'], 'action': 'check oil level'},
-        {'condition': ['oil level low'], 'action': 'add oil'},
-        {'condition': ['oil leak'], 'action': 'check engine gasket'},
-        {'condition': ['brakes squeaking'], 'action': 'replace brake pads'},
-        {'condition': ['brakes not responding'], 'action': 'check brake fluid'},
-        {'condition': ['brake fluid low'], 'action': 'add brake fluid'},
-        {'condition': ['steering wheel vibrating'], 'action': 'balance wheels'},
-        {'condition': ['car pulls to one side'], 'action': 'check wheel alignment'},
-        {'condition': ['check engine light on'], 'action': 'check engine error codes'},
-        {'condition': ['engine overheating'], 'action': 'check coolant level'},
-        {'condition': ['coolant level low'], 'action': 'add coolant'}
-    ],
-    'facts': {
-        'engine starts': False,
-        'fuel tank not empty': False,
-        'fuel tank empty': False,
-        'fuel pump working': False,
-        'air filter clean': False,
-        'air filter dirty': False,
-        'fuel pump not working': False,
-        'engine does not start': False,
-        'battery faulty': False,
-        'oil light on': False,
-        'oil level low': False,
-        'oil leak': False,
-        'brakes squeaking': False,
-        'brakes not responding': False,
-        'brake fluid low': False,
-        'steering wheel vibrating': False,
-        'car pulls to one side': False,
-        'check engine light on': False,
-        'engine overheating': False,
-        'coolant level low': False
-    }
-}
+# Knowledge base
+rules = [
+    (['check engine light is on', 'car wont start'], 'Dead battery'),
+    (['car wont start', 'clicking sound'], 'Bad starter motor'),
+    (['car wont start', 'no sound'], 'Bad battery connection'),
+    (['car stalls or dies while driving', 'car wont restart'], 'Faulty alternator'),
+    (['car shakes or vibrates', 'steering wheel vibrates'], 'Wheel balance problem'),
+    (['car makes a squealing noise', 'hard to steer'], 'Loose or worn belt'),
+    (['car makes a grinding noise', 'brake pedal pulsates'], 'Worn brake pads'),
+    (['car makes a clicking noise', 'car wont start'], 'Bad starter solenoid'),
+    (['car wont start', 'engine cranks'], 'Fuel system problem'),
+    (['car wont start', 'engine does not crank'], 'Starter motor problem'),
+    (['car runs rough', 'check engine light is on'], 'Misfiring cylinder'),
+    (['car stalls or dies while driving', 'check engine light is on'], 'Faulty oxygen sensor'),
+    (['car pulls to one side', 'tire wear is uneven'], 'Wheel alignment problem'),
+    (['car pulls to one side', 'steering wheel off-center'], 'Wheel alignment problem'),
+    (['car makes a whining noise', 'steering feels heavy'], 'Low power steering fluid'),
+    (['car makes a rattling noise', 'car is idling roughly'], 'Loose or damaged muffler'),
+    (['car makes a hissing noise', 'car overheats'], 'Coolant leak'),
+    (['brake pedal goes all the way to the floor', 'brake warning light is on'], 'Brake fluid leak'),
+    (['brake pedal feels spongy', 'car pulls to one side when braking'], 'Worn brake caliper'),
+    (['car makes a clicking noise when turning', 'car feels loose when turning'], 'Worn ball joint'),
+    (['car makes a grinding noise when turning', 'car feels loose when  turning'], 'Worn wheel bearing'),
+    (['car makes a popping noise when turning', 'steering wheel feels loose'], 'Worn tie rod end'),
+    (['car makes a clunking noise over bumps', 'car bounces excessively'], 'Worn shock absorber')
+ 
+]
 
-# define working memory
-working_memory = knowledge_base['facts']
+# Working memory and agenda
+working_memory = []
+agenda = []
 
-# define inference engine
-def inference_engine(knowledge_base):
-    agenda = []
-    for rule in knowledge_base['rules']:
-        if all(knowledge_base['facts'][condition] for condition in rule['condition']):
-            agenda.append(rule['action'])
-    return agenda
+# Inference engine
+def backward_chaining(symptoms):
+    global working_memory, agenda
+    
+    working_memory.extend(symptoms)
+    agenda.extend(symptoms)
+    
+    while agenda:
+        symptom = agenda.pop()
+        
+        for rule in rules:
+            if symptom in rule[0]:
+                premise = [s for s in rule[0] if s != symptom]
+                
+                if all(p in working_memory for p in premise):
+                    working_memory.append(rule[1])
+                    agenda.append(rule[1])
+    
+    return working_memory
 
-# define API endpoint
+# Endpoint
 @app.route('/infer', methods=['POST'])
-def car_troubleshooter():
-    data = request.get_json()
-    input_data = data['input']
-    if input_data in working_memory:
-        working_memory[input_data] = True
-    agenda = inference_engine(knowledge_base)
-    return jsonify({'actions': agenda})
+def diagnose():
+    symptoms = request.json['symptoms']
+    diagnosis = backward_chaining(symptoms)
+    return jsonify({'diagnosis': diagnosis})
 
 if __name__ == '__main__':
     app.run()
